ea37f3ff223a06db13d2490c6217a184159ee32b
Gabriel Lima <gabriel1277lima@gmail.com>
2023-08-09 01:18:54 -0300

    Feita a parte 'olá'

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import "./index.css";
 import Video from "../src/assets/space.mp4";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars } from "@fortawesome/free-solid-svg-icons";
-
+import { faBars, faArrowUp } from "@fortawesome/free-solid-svg-icons";
+import ReactLogo from "../src/assets/reactbw.png";
+import JavaScriptLogo from "../src/assets/javascriptbw.png";
+import JavaLogo from "../src/assets/javabw.png";
 const BackgroundVideo = () => {
   return (
     <video
@@ -18,32 +20,48 @@ const BackgroundVideo = () => {
 function App() {
   return (
     <>
-      {/* Header */}
-      <div className="fixed w-full">
-        {" "}
-        <div className="bg-midnightpurple/60 flex justify-center items-center h-8">
-          <FontAwesomeIcon
-            icon={faBars}
-            className="absolute left-0 ml-10 text-white text-sm  transition ease-in-out active:text-black duration-300 cursor-pointer"
-          />
-          <h1 className="z-10 text-white font-thin cursor-pointer">
-            {" "}
-            Gabi Developer
+      <div className="h-screen">
+        {/* Header */}
+        <div className="fixed w-full h-screen">
+          {" "}
+          <div className="bg-midnightpurple/60 flex justify-center items-center h-8">
+            <FontAwesomeIcon
+              icon={faBars}
+              className="absolute left-0 ml-10 text-white text-sm  transition ease-in-out active:text-black duration-300 cursor-pointer"
+            />
+            <h1 className="z-10 text-white font-thin cursor-pointer">
+              {" "}
+              Gabi Developer
+            </h1>
+          </div>
+        </div>
+
+        {/* BackgroundVideo and Text */}
+        <BackgroundVideo />
+
+        <div className="flex justify-center items-center h-80 flex-col ">
+          <h1 className="text-white text-3xl font-extrabold transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 cursor-default	">
+            The Right Dev for You.
           </h1>
+          <h2 className="text-gray-200 cursor-default text-2xl	"> Front-End</h2>
+        </div>
+        <div className="bg-gradient-to-t from-black w-full h-full p-44 absolute bottom-0 -z-10 ">
+          {" "}
+        </div>
+        <div className=" w-full text-white flex justify-center space-x-4 ">
+          <img src={ReactLogo} className="w-8"></img>
+          <img src={JavaScriptLogo} className="w-8 rounded-md"></img>
+          <img src={JavaLogo} className="w-8 "></img>
+        </div>
+        <div className="text-white flex flex-col justify-center items-center absolute bottom-1 w-full">
+          <FontAwesomeIcon icon={faArrowUp} />
+          <h1> Projects </h1>
+          <div> </div>
         </div>
       </div>
-
-      {/* BackgroundVideo and Text */}
-      <BackgroundVideo />
-      <div className="flex justify-center items-center h-80 flex-col">
-        <h1 className="text-white text-4xl font-extrabold transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 cursor-default	">
-          The Right Dev for You.
-        </h1>
-        <h2 className="text-gray-200 cursor-default text-2xl	"> Front-End</h2>
-      </div>
-      <div className="bg-gradient-to-t from-black w-full h-full p-44 absolute bottom-0 -z-10"></div>
-      <div className=" w-full text-white flex justify-center mt-80">
-        <h1 className="font-bold "> HTML, CSS, JS </h1>
+      <div className="h-screen bg-gradient-to-b from-black">
+        {" "}
+        <h1> Olá ! </h1>
       </div>
     </>
   );
